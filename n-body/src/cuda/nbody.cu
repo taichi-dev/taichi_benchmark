86eed5535879ac74f82eebca80764faabbad786a
Haidong Lan <haidonglan@taichi.graphics>
2022-03-17 15:21:15 +0800

    Update cuda print and the benchmark scripts.

--- n-body/src/cuda/nbody.cu
+++ n-body/src/cuda/nbody.cu
@@ -36,18 +36,17 @@ void bodyForce(Body *p, float dt, int n) {
 }
 
 int main(const int argc, const char** argv) {
-  
   int nBodies = 30000;
   if (argc > 1) nBodies = atoi(argv[1]);
   
   const float dt = 0.01f; // time step
   const int nIters = 10;  // simulation iterations
 
-  int bytes = nBodies*sizeof(Body);
-  float *buf = (float*)malloc(bytes);
+  int bytes = nBodies * sizeof(Body);
+  float *buf = (float*) malloc(bytes);
   Body *p = (Body*)buf;
 
-  randomizeBodies(buf, 6*nBodies); // Init pos / vel data
+  randomizeBodies(buf, 6 * nBodies); // Init pos / vel data
 
   float *d_buf;
   cudaMalloc(&d_buf, bytes);
@@ -57,8 +56,8 @@ int main(const int argc, const char** argv) {
   double totalTime = 0.0; 
 
   for (int iter = 1; iter <= nIters; iter++) {
-    StartTimer();
-
+    Timer tmr;
+    tmr.start();
     cudaMemcpy(d_buf, buf, bytes, cudaMemcpyHostToDevice);
     bodyForce<<<nBlocks, BLOCK_SIZE>>>(d_p, dt, nBodies); // compute interbody forces
     cudaMemcpy(buf, d_buf, bytes, cudaMemcpyDeviceToHost);
@@ -68,23 +67,18 @@ int main(const int argc, const char** argv) {
       p[i].y += p[i].vy*dt;
       p[i].z += p[i].vz*dt;
     }
-
-    const double tElapsed = GetTimer() / 1000.0;
+    tmr.stop();
+    const double tElapsed = tmr.getTimeMillisecond();
     if (iter > 1) { // First iter is warm up
       totalTime += tElapsed; 
     }
-#ifndef SHMOO
-    printf("Iteration %d: %.3f seconds\n", iter, tElapsed);
-#endif
   }
   double avgTime = totalTime / (double)(nIters-1); 
 
-#ifdef SHMOO
-  printf("%d, %0.3f\n", nBodies, 1e-9 * nBodies * nBodies / avgTime);
+#ifdef JSON_OUTPUT
+  printf("{\"nbodies\":%d, \"rate\":%.3lf, \"time\": %.3lf}\n", nBodies, 1e-6 * nBodies * nBodies / avgTime, avgTime);
 #else
-  printf("Average rate for iterations 2 through %d: %.3f +- %.3f steps per second.\n",
-         nIters, rate);
-  printf("%d Bodies: average %0.3f Billion Interactions / second\n", nBodies, 1e-9 * nBodies * nBodies / avgTime);
+  printf("%d Bodies: average %0.3f Billion Interactions / second\n", nBodies, 1e-6 * nBodies * nBodies / avgTime);
 #endif
   free(buf);
   cudaFree(d_buf);
