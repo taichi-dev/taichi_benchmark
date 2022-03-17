7a920a93c743e940de392a1bf7f060256427098c
Haidong Lan <haidonglan@taichi.graphics>
2022-03-17 14:42:24 +0800

    Update var name

--- saxpy/src/cuda/thrust.cu
+++ saxpy/src/cuda/thrust.cu
@@ -12,7 +12,7 @@ __host__ static __inline__ float rand_01()
     return ((float)rand()/RAND_MAX);
 }
 
-template<int fma_factor>
+template<int nesting_factor>
 int saxpy(int _N) {
     int N = _N * _N;
     thrust::host_vector<float> x(N), y(N);
@@ -26,26 +26,26 @@ int saxpy(int _N) {
     size_t nIter = 5000;
     for (int i = 0; i < nIter; ++i) {
         // Perform SAXPY on 1M elements
-        if (fma_factor == 1) { 
+        if (nesting_factor == 1) { 
             thrust::transform(d_x.begin(), d_x.end(), d_y.begin(), d_y.begin(), 2.0f * _1 + _2);
-        } else if (fma_factor == 2) {
+        } else if (nesting_factor == 2) {
             thrust::transform(d_x.begin(), d_x.end(), d_y.begin(), d_y.begin(), 2.0f * (2.0f * _1 + _2) + _2);
-        } else if (fma_factor == 4) {
+        } else if (nesting_factor == 4) {
             thrust::transform(d_x.begin(), d_x.end(), d_y.begin(), d_y.begin(), 2.0f * (2.0f * (2.0f * (2.0f * _1 + _2) + _2) + _2) + _2);
-        } else if (fma_factor == 8) {
+        } else if (nesting_factor == 8) {
             thrust::transform(d_x.begin(), d_x.end(), d_y.begin(), d_y.begin(), 2.0f * (2.0f * (2.0f * (2.0f * (2.0f * (2.0f * (2.0f * (2.0f * _1 + _2) + _2) + _2) + _2) + _2) + _2) + _2) + _2);
-        } else if (fma_factor == 16) {
+        } else if (nesting_factor == 16) {
             thrust::transform(d_x.begin(), d_x.end(), d_y.begin(), d_y.begin(), 2.0f * (2.0f * (2.0f * (2.0f * (2.0f * (2.0f * (2.0f * (2.0f * (2.0f * (2.0f * (2.0f * (2.0f * (2.0f * (2.0f * (2.0f * (2.0f * _1 + _2) + _2) + _2) + _2) + _2) + _2) + _2) + _2) + _2) + _2) + _2) + _2) + _2) + _2) + _2) + _2);
         } 
     }
     tmr.stop();
     double avg_time = tmr.getTimeMillisecond() / nIter;
-    double GFlops = 1e-6 * N * 2 * fma_factor / avg_time;
+    double GFlops = 1e-6 * N * 2 * nesting_factor / avg_time;
     double GBs = 1e-6 * N * sizeof(float) * 3 / avg_time;
 #ifdef JSON_OUTPUT
-    printf("{\"N\": %d, \"fold\":%d, \"time\":%.3lf, \"gflops\":%.3lf, \"gbs\": %.3lf}\n",  _N, fma_factor, avg_time, GFlops, GBs);
+    printf("{\"N\": %d, \"fold\":%d, \"time\":%.3lf, \"gflops\":%.3lf, \"gbs\": %.3lf}\n",  _N, nesting_factor, avg_time, GFlops, GBs);
 #else
-    printf("%dx%d@%d, %.3lf ms, %.3lf GFLOPS, %.3lf GB/s\n", _N, _N, fma_factor, avg_time, GFlops, GBs);
+    printf("%dx%d@%d, %.3lf ms, %.3lf GFLOPS, %.3lf GB/s\n", _N, _N, nesting_factor, avg_time, GFlops, GBs);
 #endif
     
     y = d_y; // copy results to the host vector
