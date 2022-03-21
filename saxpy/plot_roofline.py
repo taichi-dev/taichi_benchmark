2045f0fa55b008f4cdb9b5ee1cb2815cb169b820
Haidong Lan <haidonglan@taichi.graphics>
2022-03-21 13:11:00 +0800

    Revise roofline plot: add vlines to prompt nesting factor

--- saxpy/plot_roofline.py
+++ saxpy/plot_roofline.py
@@ -3,46 +3,48 @@ from src.taichi.benchmark import benchmark as benchmark_taichi
 import math
 import os
 
-def plot_roofline_log_scale(ax):
+def plot_nesting_factor_lines(ax, ylimit, nesting_factors=[]):
+    print(nesting_factors)
+    for nf in nesting_factors:
+        print(nf)
+        print(nf / 6.0 * 760)
+        #ax.vlines(x=[f / 6.0 for f in nesting_factors], ymin=0, ymax=nf / 6.0  * 760)
+        ax.vlines(x=[nf/6.0] , ymin=0, ymax=min(nf / 6.0  * 760, 29700), label="TEST", color='grey', linestyle='dashed')
+        ax.text(nf / 6.0, 40, "Nest={}".format(nf), rotation=270, verticalalignment='top')
+
+def plot_roofline(ax, scale='log'):
     bw = 760
     cc = 29700
     ridge = cc / 760.0
-    xlimit = 1e4
-    ax.plot([0, ridge, xlimit], [0, cc, cc], color='black')
-    ax.set_xscale('log', base=10)
-    ax.set_yscale('log', base=10)
+    membound_str = "DRAM bandwidth=760 GB/s"
+    compbound_str = "FP32 peak={} GFLOPs".format(cc)
+    if scale == 'log':
+        xlimit = 1e4
+        ylimit = cc * 2
+        plot_nesting_factor_lines(ax, ylimit, [2**x for x in range(10)])
+        ax.set_xscale('log', base=10)
+        ax.set_yscale('log', base=10)
+        ax.text(0.09, 0.95, membound_str, fontsize=12, rotation = 37, transform=ax.transAxes,
+            verticalalignment='top')
+        ax.text(0.55, 0.98, compbound_str, fontsize=12, transform=ax.transAxes,
+            verticalalignment='top')
+    elif scale == 'linear':
+        xlimit = 100
+        ylimit = cc * 1.2
+        ax.text(0.05, 0.75, membound_str, fontsize=12, rotation = 57, transform=ax.transAxes,
+            verticalalignment='top')
+        ax.text(0.55, 0.88, compbound_str, fontsize=12, transform=ax.transAxes,
+            verticalalignment='top')
+    else:
+        raise Exception("Unknown plot scale {}".format(scale))
     ax.set_xlim([2**-3, xlimit])
-    ax.set_ylim([1, cc * 2])
+    ax.set_ylim([1, ylimit])
+    ax.plot([0, ridge, xlimit], [0, cc, cc], color='black')
+    ax.grid(True, "minor")
+    props = dict(boxstyle='round', facecolor='wheat', alpha=0.2)
     ax.set_xlabel("Arithmetic Intensity")
     ax.set_ylabel("Performance (GFLOPS)")
     ax.set_title("Taichi nested SAXPY roofline plot on 4096x4096 arrays")
-    ax.grid(True, "minor")
-    membound_str = "DRAM bandwidth=760 GB/s"
-    compbound_str = "FP32 peak={} GFLOPs".format(cc)
-    props = dict(boxstyle='round', facecolor='wheat', alpha=0.2)
-    ax.text(0.09, 0.95, membound_str, fontsize=12, rotation = 37, transform=ax.transAxes,
-            verticalalignment='top')
-    ax.text(0.55, 0.98, compbound_str, fontsize=12, transform=ax.transAxes,
-            verticalalignment='top')
-
-def plot_roofline_linear_scale(ax):
-    bw = 760
-    cc = 29700
-    ridge = cc / 760.0
-    xlimit = 100
-    ax.plot([0, ridge, xlimit], [0, cc, cc], color='black')
-    #ax.set_xscale('log', base=10)
-    #ax.set_yscale('log', base=10)
-    ax.set_xlim([2**-3, xlimit])
-    ax.set_ylim([1, cc * 1.2])
-    ax.grid(True, "minor")
-    membound_str = "DRAM bandwidth=760 GB/s"
-    compbound_str = "FP32 peak={} GFLOPs".format(cc)
-    props = dict(boxstyle='round', facecolor='wheat', alpha=0.2)
-    ax.text(0.05, 0.75, membound_str, fontsize=12, rotation = 57, transform=ax.transAxes,
-            verticalalignment='top')
-    ax.text(0.55, 0.88, compbound_str, fontsize=12, transform=ax.transAxes,
-            verticalalignment='top')
 
 def get_arithmetic_intensity(flops, bw):
     return flops / bw
@@ -50,6 +52,7 @@ def get_arithmetic_intensity(flops, bw):
 def get_color_marker(N):
     color_dict = {256:'gold', 512:'darkorange', 1024:'green', 2048:'blue', 4096:'red', 8192:'darkgrey',}
     return color_dict[N]
+
 def plot_results(ax, results):
     x = []
     y = []
@@ -70,12 +73,12 @@ if __name__ == '__main__':
         pass
     taichi_results = benchmark_taichi(max_nesting=512)
     fig, ax = plt.subplots()
-    plot_roofline_linear_scale(ax)
-    plot_results(ax, taichi_results)
-    fig.savefig('fig/roofline_linear_scale.png',dpi=200)
-    fig.clf()
-    ax.cla()
-    fig, ax = plt.subplots()
-    plot_roofline_log_scale(ax)
+    #plot_roofline(ax, scale='linear')
+    #plot_results(ax, taichi_results)
+    #fig.savefig('fig/roofline_linear_scale.png',dpi=200)
+    #fig.clf()
+    #ax.cla()
+    #fig, ax = plt.subplots()
+    plot_roofline(ax, scale='log')
     plot_results(ax, taichi_results)
     fig.savefig('fig/roofline_log_scale.png',dpi=200)
