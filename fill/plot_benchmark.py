fa25206526ace59674f1d34aacaa963e8c20d4dd
Haidong Lan <haidonglan@taichi.graphics>
2022-04-06 16:18:18 +0800

    Update plot script for fill

--- fill/plot_benchmark.py
+++ fill/plot_benchmark.py
@@ -4,7 +4,7 @@ from src.cuda.benchmark import benchmark as benchmark_cuda
 import sys
 import os
 
-sample_res = {'cuda_kernel': [{'N': 2097152, 'time': 0.013, 'bandwidth': 632.938}, {'N': 4194304, 'time': 0.025, 'bandwidth': 678.134}, {'N': 8388608, 'time': 0.048, 'bandwidth': 701.28}, {'N': 16777216, 'time': 0.094, 'bandwidth': 712.921}, {'N': 33554432, 'time': 0.187, 'bandwidth': 718.698}, {'N': 67108864, 'time': 0.372, 'bandwidth': 721.962}, {'N': 134217728, 'time': 0.742, 'bandwidth': 723.445}, {'N': 268435456, 'time': 1.483, 'bandwidth': 724.197}], 'cuda_memset': [{'N': 2097152, 'time': 0.013, 'bandwidth': 639.448}, {'N': 4194304, 'time': 0.025, 'bandwidth': 676.533}, {'N': 8388608, 'time': 0.048, 'bandwidth': 700.231}, {'N': 16777216, 'time': 0.094, 'bandwidth': 712.038}, {'N': 33554432, 'time': 0.187, 'bandwidth': 718.778}, {'N': 67108864, 'time': 0.372, 'bandwidth': 721.81}, {'N': 134217728, 'time': 0.742, 'bandwidth': 723.387}, {'N': 268435456, 'time': 1.483, 'bandwidth': 724.223}], 'taichi_fill': [{'N': 2097152, 'time': 0.013391969660297036, 'bandwidth': 626.3909053549886}, {'N': 4194304, 'time': 0.024852906176820396, 'bandwidth': 675.0605293656818}, {'N': 8388608, 'time': 0.04924052860587835, 'bandwidth': 681.4393133057117}, {'N': 16777216, 'time': 0.09494293201714754, 'bandwidth': 706.8337007738453}, {'N': 33554432, 'time': 0.18779275473207235, 'bandwidth': 714.7119610204935}, {'N': 67108864, 'time': 0.3734815325587988, 'bandwidth': 718.7382309398097}, {'N': 134217728, 'time': 0.7453848794102669, 'bandwidth': 720.2599983310114}, {'N': 268435456, 'time': 1.4887062897905707, 'bandwidth': 721.2583377685954}]}
+sample_res = {'cuda_kernel': [{'N': 2097152, 'time': 0.013, 'bandwidth': 632.447}, {'N': 4194304, 'time': 0.025, 'bandwidth': 678.898}, {'N': 8388608, 'time': 0.048, 'bandwidth': 701.305}, {'N': 16777216, 'time': 0.094, 'bandwidth': 712.983}, {'N': 33554432, 'time': 0.187, 'bandwidth': 718.707}, {'N': 67108864, 'time': 0.372, 'bandwidth': 721.921}, {'N': 134217728, 'time': 0.742, 'bandwidth': 723.445}, {'N': 268435456, 'time': 1.483, 'bandwidth': 724.217}], 'cuda_memset': [{'N': 2097152, 'time': 0.013, 'bandwidth': 639.007}, {'N': 4194304, 'time': 0.025, 'bandwidth': 677.286}, {'N': 8388608, 'time': 0.048, 'bandwidth': 699.962}, {'N': 16777216, 'time': 0.094, 'bandwidth': 712.255}, {'N': 33554432, 'time': 0.187, 'bandwidth': 718.809}, {'N': 67108864, 'time': 0.372, 'bandwidth': 721.826}, {'N': 134217728, 'time': 0.742, 'bandwidth': 723.365}, {'N': 268435456, 'time': 1.483, 'bandwidth': 724.031}], 'taichi_fill': [{'N': 2097152, 'time': 0.013420191397890448, 'bandwidth': 625.0736484517371}, {'N': 4194304, 'time': 0.024879930317401888, 'bandwidth': 674.3272905497421}, {'N': 8388608, 'time': 0.05017550475895405, 'bandwidth': 668.7412944064515}, {'N': 16777216, 'time': 0.09498817566782236, 'bandwidth': 706.4970300585887}, {'N': 33554432, 'time': 0.18780816067010164, 'bandwidth': 714.653333066623}, {'N': 67108864, 'time': 0.3733440497890115, 'bandwidth': 719.0029040283389}, {'N': 134217728, 'time': 0.7452589934691787, 'bandwidth': 720.3816615494532}, {'N': 268435456, 'time': 1.4891986697912216, 'bandwidth': 721.0198651000228}]}
 
 def run_benchmarks():
     cuda_results = benchmark_cuda()
@@ -47,8 +47,8 @@ def create_bar_plot(results):
     label_font_size = 14
     ax.legend(['Taichi', 'CUDA (Kernel)', 'CUDA (cuMemset)'])
     ax.set_ylabel("Bandwidth (GB/s)", fontsize=label_font_size)
-    ax.set_xlabel("#Elements", fontsize=label_font_size)
-    ax.set_title("Fill bandwidth Benchmark", fontsize=title_font_size)
+    ax.set_xlabel("Array Size", fontsize=label_font_size)
+    ax.set_title("Array Fill Benchmark", fontsize=title_font_size)
     ax.set_ylim([0, 850])
     ax.axhline(y = 760, color='grey', linestyle = 'dashed')
     ax.text(ax.get_xlim()[1] * 0.6, 770, 'DRAM Bandwidth=760GB/s')
