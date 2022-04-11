c9c7deba3245037c835e229d7dc01e916db54768
Haidong Lan <haidonglan@taichi.graphics>
2022-04-11 10:15:04 +0800

    Update P2G scripts

---
+++ p2g/misc.py
@@ -0,0 +1,56 @@
+cuda_sample_results = {
+    'cuda_baseline': [{
+        'n_particles': 512,
+        'time_ms': 0.335444
+    }, {
+        'n_particles': 2048,
+        'time_ms': 0.320878
+    }, {
+        'n_particles': 4608,
+        'time_ms': 0.450095
+    }, {
+        'n_particles': 8192,
+        'time_ms': 0.472582
+    }, {
+        'n_particles': 12800,
+        'time_ms': 0.572223
+    }, {
+        'n_particles': 18432,
+        'time_ms': 0.822029
+    }, {
+        'n_particles': 25088,
+        'time_ms': 1.023057
+    }, {
+        'n_particles': 32768,
+        'time_ms': 1.22928
+    }]
+}
+
+taichi_sample_results = {
+    'taichi_baseline': [{
+        'n_particles': 512,
+        'time_ms': 0.5216462373027753
+    }, {
+        'n_particles': 2048,
+        'time_ms': 0.5243271660049231
+    }, {
+        'n_particles': 4608,
+        'time_ms': 0.546237979492048
+    }, {
+        'n_particles': 8192,
+        'time_ms': 0.5765281875085293
+    }, {
+        'n_particles': 12800,
+        'time_ms': 0.621946640620763
+    }, {
+        'n_particles': 18432,
+        'time_ms': 0.8157364931662414
+    }, {
+        'n_particles': 25088,
+        'time_ms': 0.9429942363254895
+    }, {
+        'n_particles': 32768,
+        'time_ms': 1.0296167158259095
+    }]
+}
+
