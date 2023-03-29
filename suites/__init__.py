a28921e4ee104f726f45766a21d68d1a749051f3
Proton <feisuzhu@163.com>
2023-03-29 14:41:01 +0800

    meh

--- suites/__init__.py
+++ suites/__init__.py
@@ -1,4 +1,3 @@
-from . import n_body, saxpy, mpm, fill, stencil
-from .AOTBench import library_size
+from . import n_body, saxpy
 
-__all__ = ['n_body', 'saxpy', 'mpm', 'stencil', 'fill', 'library_size']
+__all__ = ['n_body', 'saxpy']
